cf79018517cd1b6cb36e0e34150140f50ec107f5
ashish1sasmal <ashishsasmal1@gmail.com>
2019-07-30 02:08:12 +0530

    third

--- top10/admin.py
+++ top10/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Album
+from .models import *
 
 class AlbumAdmin(admin.ModelAdmin):
 	list_display=['name','artist','link']
 
+
+class UserInfoAdmin(admin.ModelAdmin):
+	list_display=['user']
+
 admin.site.register(Album,AlbumAdmin)
+admin.site.register(UserInfo,UserInfoAdmin)
--- top10/models.py
+++ top10/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Album(models.Model):
 	name=models.CharField(max_length=50)
 	artist=models.CharField(max_length=50)
 	link=models.URLField()
+
+class UserInfo(models.Model):
+	user=models.OneToOneField(User,on_delete=models.CASCADE)
+	picture=models.ImageField(blank=True,upload_to='profile_pics')
